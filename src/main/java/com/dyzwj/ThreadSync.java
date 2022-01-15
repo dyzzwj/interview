5c458f2c409a357a3b5cc062acc0af0c4efb49ee
dyzzwj <dyzwj1998@163.com>
2022-01-15 09:19:40 +0800

    aa

---
+++ src/main/java/com/dyzwj/ThreadSync.java
@@ -0,0 +1,18 @@
+package com.dyzwj;
+
+public class ThreadSync {
+
+    /**
+     * 线程同步机制：
+     *  synchorized
+     *  lock
+     *  cas和volatile
+     *  ThreadLocal
+     *  阻塞队列
+     *
+     *
+     *
+     */
+
+
+}
---
+++ src/main/java/com/dyzwj/ThreadTransport.java
@@ -0,0 +1,15 @@
+package com.dyzwj;
+
+public class ThreadTransport {
+
+    /**
+     * 线程通信机制
+     *  wait notify
+     *  await signal
+     *  countdownlatch
+     *  cyclicbarrier
+     *  semaphore
+     *
+     *
+     */
+}
